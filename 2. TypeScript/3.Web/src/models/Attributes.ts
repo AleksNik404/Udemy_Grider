09ffba54878956c9b78d5706483bcd245a3f4b92
AleksNik404 <AleksNik404@gmail.com>
2022-11-15 13:22:45 +0300

    TypeScript: feat: attributes class and refactoring

--- 2. TypeScript/3.Web/src/models/Attributes.ts
+++ 2. TypeScript/3.Web/src/models/Attributes.ts
@@ -1,11 +1,22 @@
+import { UserProps } from './User';
+
 export class Attributes<T> {
   constructor(private data: T) {}
 
-  get(propName: string): number | string {
-    return this.data[propName];
+  get<K extends keyof T>(key: K): T[K] {
+    return this.data[key];
   }
 
   set(update: T): void {
     this.data = { ...this.data, ...update };
   }
 }
+
+const attrs = new Attributes<UserProps>({ id: 5, name: 'adsf', age: 20 });
+
+const name = attrs.get('name');
+const age = attrs.get('age');
+const id = attrs.get('id');
+
+// const attrs = new Attributes<UserProps>({ id: 5, name: 'adsf', age: 20 });
+// const id: number = attrs.get('id') as number;
